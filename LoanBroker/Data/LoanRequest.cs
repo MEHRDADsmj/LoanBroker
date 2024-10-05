69dc9fa1c3fc15fe02968f07c07983dd09acda33
Mehrdad Jafari <59224497+MEHRDADsmj@users.noreply.github.com>
2024-10-05 22:41:05 +0330

    Add request and response data structures

---
+++ LoanBroker/Data/LoanRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace LoanBroker.Data;
+
+public class LoanRequest
+{
+    public string BankName { get; set; } = null!;
+    public long Amount { get; set; }
+}
---
+++ LoanBroker/Data/LoanResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace LoanBroker.Data;
+
+public class LoanResponse
+{
+    public List<long> Installments { get; set; } = new List<long>();
+}

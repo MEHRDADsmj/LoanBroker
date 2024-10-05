258999fb4cbe8c33f776ecbef3560f79e60d274b
Mehrdad Jafari <59224497+MEHRDADsmj@users.noreply.github.com>
2024-10-05 23:38:47 +0330

    Add bank A provider logic and tests

---
+++ BankAProvider/BankAProvider.cs
@@ -0,0 +1,48 @@
+﻿using LoanBroker.Data;
+
+namespace BankAProvider;
+
+public class BankAProvider : IBankProvider.IBankProvider
+{
+    public readonly float interestRate = 0.2f;
+    private int numberOfInstallments = 10;
+    public readonly long loanAmountUpperLimit = 999999999999;
+    
+    public LoanResponse CalculateLoan(LoanRequest loan)
+    {
+        if (IsAmountInBounds(loan.Amount))
+        {
+            throw new ArgumentOutOfRangeException(nameof(loan), "loan.Amount is less than 0");
+        }
+        else
+        {
+            var totalValue = CalculateTotalLoanAndInterest(loan.Amount);
+            return new LoanResponse()
+                   {
+                       Installments = ExtractInstallments(totalValue),
+                   };
+        }
+    }
+    
+    private bool IsAmountInBounds(long amount)
+    {
+        return amount < 0.0f || amount > loanAmountUpperLimit;
+    }
+
+    private long CalculateTotalLoanAndInterest(long amount)
+    {
+        return (long)(amount * (interestRate + 1.0f));
+    }
+
+    private List<long> ExtractInstallments(long totalAmount)
+    {
+        var installmentAmount = totalAmount / numberOfInstallments;
+        List<long> installments = new List<long>();
+        for (int i = 0; i < numberOfInstallments; i++)
+        {
+            installments.Add(installmentAmount);
+        }
+
+        return installments;
+    }
+}
---
+++ BankProviders.Tests/testBankAProvider.cs
@@ -0,0 +1,54 @@
+using LoanBroker.Data;
+
+namespace BankProviders.Tests
+{
+    public class testBankAProvider
+    {
+        private BankAProvider.BankAProvider provider;
+        
+        [SetUp]
+        public void Setup()
+        {
+            provider = new BankAProvider.BankAProvider();
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(-1)]
+        [TestCase(1000)]
+        [TestCase(long.MaxValue)]
+        [TestCase(long.MinValue)]
+        public void testCalculateLoan(long amount)
+        {
+            var loan = PrepareLoan(amount);
+            try
+            {
+                var response = provider.CalculateLoan(loan);
+                long sum = response.Installments.Sum();
+
+                if (Math.Abs(sum - amount * (1 + provider.interestRate)) < 1.0f)
+                {
+                    Assert.Pass();
+                }
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e);
+                if (amount < 0 || amount >= provider.loanAmountUpperLimit)
+                {
+                    Assert.Pass();
+                }
+            }
+
+            Assert.Fail();
+        }
+
+        private LoanRequest PrepareLoan(long amount)
+        {
+            return new LoanRequest()
+            {
+                Amount = amount,
+            };
+        }
+    }
+}

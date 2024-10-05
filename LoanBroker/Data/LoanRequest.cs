1a1b0865d7be6aed0b24aa7090dcfcc4ed4ad5e8
Mehrdad Jafari <59224497+MEHRDADsmj@users.noreply.github.com>
2024-10-05 23:37:28 +0330

    Make loan request fields required

--- LoanBroker/Data/LoanRequest.cs
+++ LoanBroker/Data/LoanRequest.cs
@@ -1,7 +1,12 @@
-﻿namespace LoanBroker.Data;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LoanBroker.Data;
 
 public class LoanRequest
 {
+    [Required]
     public string BankName { get; set; } = null!;
+    
+    [Required]
     public long Amount { get; set; }
 }

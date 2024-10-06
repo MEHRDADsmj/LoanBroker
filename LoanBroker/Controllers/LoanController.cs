029faa49cec63d75a315509023c563f2384ba604
Mehrdad Jafari <59224497+MEHRDADsmj@users.noreply.github.com>
2024-10-06 15:49:35 +0330

    Basic implementation of the controller

---
+++ LoanBroker/Controllers/LoanController.cs
@@ -0,0 +1,23 @@
+﻿using LoanBroker.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoanBroker.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class LoanController : ControllerBase
+{
+    private IBankProvider.IBankProvider bankProvider;
+
+    public LoanController()
+    {
+        
+    }
+    
+    [HttpGet("/")]
+    public async Task<ActionResult<LoanResponse>> RequestLoan(LoanRequest loanRequest)
+    {
+        var resp = bankProvider.CalculateLoan(loanRequest);
+        return Ok(resp);
+    }
+}

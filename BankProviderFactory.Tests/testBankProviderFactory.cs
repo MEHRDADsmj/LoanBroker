e666c52b47249204cc15c597ddd4b66867862513
Mehrdad Jafari <59224497+MEHRDADsmj@users.noreply.github.com>
2024-10-06 19:25:27 +0330

    Add bank provider factory

---
+++ BankProviderFactory.Tests/testBankProviderFactory.cs
@@ -0,0 +1,42 @@
+namespace BankProviderFactory.Tests;
+
+public class testBankProviderFactory
+{
+    private BankProviderFactory factory;
+    
+    [SetUp]
+    public void Setup()
+    {
+        factory = new BankProviderFactory();
+        factory.Init();
+    }
+
+    [Test]
+    [TestCase("bank_a")]
+    [TestCase("Bank_A")]
+    [TestCase("BANK_A")]
+    public void testGetBankProviderSuccess(string bankName)
+    {
+        var provider = factory.GetBankProvider(bankName);
+        Assert.IsNotNull(provider);
+    }
+    
+    [Test]
+    [TestCase("")]
+    [TestCase(" ")]
+    [TestCase("loremIpsum")]
+    public void testGetBankProviderFailure(string bankName)
+    {
+        try
+        {
+            var provider = factory.GetBankProvider(bankName);
+        }
+        catch (KeyNotFoundException e)
+        {
+            Console.WriteLine(e);
+            Assert.Pass();
+        }
+
+        Assert.Fail();
+    }
+}
---
+++ BankProviderFactory/BankProviderFactory.cs
@@ -0,0 +1,25 @@
+﻿namespace BankProviderFactory;
+
+public class BankProviderFactory
+{
+    private Dictionary<string, IBankProvider.IBankProvider> providerDict;
+
+    public void Init()
+    {
+        providerDict = new Dictionary<string, IBankProvider.IBankProvider>
+                       {
+                           { "bank_a", new BankAProvider.BankAProvider() },
+                           { "bank_b", new BankBProvider.BankBProvider() },
+                       };
+    }
+
+    public IBankProvider.IBankProvider GetBankProvider(string bankName)
+    {
+        if (providerDict.ContainsKey(bankName.ToLower()))
+        {
+            return providerDict[bankName.ToLower()];
+        }
+
+        throw new KeyNotFoundException("Invalid Bank name");
+    }
+}
